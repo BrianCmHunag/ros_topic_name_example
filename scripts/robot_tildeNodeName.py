28ba1c159ebcb626de9aab8b82ccde110c4f4cbf
BrianCmHunag <iles88039@gmail.com>
2020-09-28 02:51:50 +0000

    add code

---
+++ scripts/robot_tildeNodeName.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import rospy 
+from geometry_msgs.msg import Pose
+
+rospy.init_node('robot', anonymous=True)
+pub = rospy.Publisher('~my_position', Pose, queue_size=1)
+rate = rospy.Rate(1) # 1hz
+
+while not rospy.is_shutdown():
+    pub.publish(Pose())
+    rate.sleep()
